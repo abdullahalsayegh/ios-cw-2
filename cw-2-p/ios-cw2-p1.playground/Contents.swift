6fcb1bad5e964f84134b119fabd11fe36ea5a250
abdullah <Ab.alsayegh2004@gmail.com>
2020-06-17 23:29:40 +0300

    p1,p2,p3 add

---
+++ cw-2-p/ios-cw2-p1.playground/Contents.swift
@@ -0,0 +1,17 @@
+let family = ["hashm", "abdullah", "abdalaziz", "yosef"]
+
+var evens = [2, 4, 6, 8, 10, 12]
+
+var grades = [87.5, 91.4, 85.4]
+
+var trueFalse = [true, true, true, false]
+
+print(family[0], family[3])
+
+print(evens[0], evens[5])
+
+print(grades[0], grades[2])
+
+print(trueFalse[0], trueFalse[3])
+print(family)
+
